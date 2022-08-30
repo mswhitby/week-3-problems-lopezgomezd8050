47fa499bf272ffcd64e635f64ee2243f4e0f1417
lopezgomezd8050 <112404788+lopezgomezd8050@users.noreply.github.com>
2022-08-30 09:09:36 -0500

    Update fizz_buzz.py

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,1 +1,13 @@
-
+def fb(n):
+  if n%3==0 and n%5==0:
+    print("Fizz Buzz")
+  elif n%3==0:
+    print("Fizz")
+  elif n%5==0:
+    print("Buzz")
+  else:
+    print(n)
+fb(3)
+fb(5)
+fb(15)
+fb(4)
