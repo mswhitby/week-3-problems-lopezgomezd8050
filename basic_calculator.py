660b0b6c3263e13ad2cdc262c7f25d0dfde1114d
lopezgomezd8050 <112404788+lopezgomezd8050@users.noreply.github.com>
2022-08-30 09:34:49 -0500

    Update basic_calculator.py

--- basic_calculator.py
+++ basic_calculator.py
@@ -1,1 +1,19 @@
+def calc(n1,o,n2):
+  if o == "+":
+    return(n1+n2)
+    
+  if o == "-":
+    return(n1-n2)
+    
+  if o == "*":
+    return(n1*n2)
+    
+  if o == "/":
+    if n2==0:
+      return("Cant divide by 0!")
+    else:
+      return(n1/n2)
+      
 
+printCalc=calc(0,"/",0)
+print(printCalc)
