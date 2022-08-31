ddd463cf902ac51bcd27a2049a9842caebe1c611
lopezgomezd8050 <112404788+lopezgomezd8050@users.noreply.github.com>
2022-08-31 09:28:43 -0500

    Update calc_damage.py

--- calc_damage.py
+++ calc_damage.py
@@ -1,1 +1,13 @@
+def damage(d,s,t):
+  if t == "second" and d>0 and s>0:
+    print(d*s*1)
+  elif t == "minute" and d>0 and s>0:
+    print(d*s*60)
+  elif t == "hour" and d>0 and s>0:
+    print(d*s*3600)
+  elif t == "day" and d>0 and s>0:
+    print(d*s*86400)
+  else:
+    print("Invalid")
 
+damage(45,1,"day")
