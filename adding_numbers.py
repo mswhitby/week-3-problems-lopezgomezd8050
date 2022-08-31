b0f579c3b4d6f187625e8dfb95cec290f8438425
lopezgomezd8050 <112404788+lopezgomezd8050@users.noreply.github.com>
2022-08-31 09:39:04 -0500

    Update adding_numbers.py

--- adding_numbers.py
+++ adding_numbers.py
@@ -1,4 +1,15 @@
+#old code
 def add(n1,n2):
   return n1+n2
 n3=add(1,6)
 print(n3)
+
+#new better code
+
+def add(n1,n2):
+  if (n1 != "" or None) and (n2 != "" or None):
+    n3=(int(n1)+int(n2))
+    print(str(n3))
+  else:
+    print("invalid operation")
+add("6","3")
